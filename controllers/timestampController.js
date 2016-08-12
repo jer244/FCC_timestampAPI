72dc86cf6056680d3d186aef97cddb9f34142ec8
Jeremy <hurleyjer@yahoo.com>
2016-08-12 19:02:38 +0000

    working server

--- controllers/timestampController.js
+++ controllers/timestampController.js
@@ -1,4 +1,5 @@
 var bodyParser = require('body-parser');
+var moment = require('moment');
 
 module.exports = function (app){
 
@@ -10,7 +11,24 @@ app.get('/', function (req, res){
 });
 
 app.get('/:id', function (req, res) {
-    res.json({parameter: req.params.id});
-});
-
+   var inputDate = req.params.id;
+   
+    //if not at number, check if string is a valid date and output accordingly
+    if (isNaN(inputDate)){
+	     if(moment(inputDate).isValid(true)){
+	      var unixDate = Math.round((new Date(inputDate).getTime())/1000);
+	      res.json({"unix": unixDate, "natural": inputDate});
+	     }else{
+	          res.json({"unix": null, "natural": null});
+	     }
+    //if number, check if valid unix date and output accordingly 
+    }else{
+	     if(moment.unix(inputDate).isValid()){
+	     var dateStr = moment.unix(inputDate).format("MMMM D, YYYY");
+	     res.json({"unix": inputDate, "natural": dateStr});
+     }else{
+	          res.json({"unix": null, "natural": null});
+	     }
+    }
+ });
 };
--- server.js
+++ server.js
@@ -11,5 +11,5 @@ timestampController(app);
 
 //listen to port 8080
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+  console.log('Timestamp API listening on port 8080!');
 });
